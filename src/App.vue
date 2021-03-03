b6b8005bbaa03873bd9a08d9dda9cd77df75d65f
des643 <2641651764@qq.com>
2021-03-03 20:21:46 +0800

    添加tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <tab-bar>
+      <tab-bar-item>
+        <template v-slot:item-text>
+          首页
+        </template>
+      </tab-bar-item>
+      <tab-bar-item>
+        <template v-slot:item-text>
+          分类
+        </template>
+      </tab-bar-item>
+      <tab-bar-item>
+        <template v-slot:item-text>
+          购物车
+        </template>
+      </tab-bar-item>
+      <tab-bar-item>
+        <template v-slot:item-text>
+          我的
+        </template>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import TabBar from './components/content/tabbar/TabBar'
+  import TabBarItem from "./components/content/tabbar/TabBarItem";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TabBar,
+    TabBarItem
   }
 }
 </script>
 
 <style>
+  @import './assets/css/base.css';
 </style>
---
+++ src/components/content/tabbar/TabBarItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="tab-bar-item">
+        <div :class="{isActived:isActive}">
+            <slot name="item-text"></slot>
+        </div>
+    </div>
+
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    data() {
+      return {
+        isActive: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    @import '../../../assets/css/base.css';
+
+    .tab-bar-item{
+        flex: 1;
+        text-align: center;
+        height: 49px;
+    }
+
+    .isActived{
+        color: red
+    }
+</style>
